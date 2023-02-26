2971349fb90b50712ff27af1580167f459fde995
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2023-02-26 15:03:51 -0600

    Update SecurityConfig.java

--- src/main/java/com/javatechie/config/SecurityConfig.java
+++ src/main/java/com/javatechie/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
 
         return http
                 .authorizeHttpRequests()
-                .requestMatchers("/home").permitAll()
+                .requestMatchers("/**").permitAll()
                 .and().build();
     }
 
